3ef0a5e6c7a3c116117bbe4a7449da650cde9890
Arnausole2004 <arnausole04@gmail.com>
2025-06-11 18:49:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-function mostrarMensaje() {
-  alert("¡Gracias por visitar esta web! Mucho éxito como estudiante emprendedor.");
-}
+AOS.init({ once:true });
+
+document.querySelectorAll('a[href^=\"#\"]').forEach(a=>{
+  a.addEventListener('click',e=>{
+    const t=document.querySelector(a.getAttribute('href'));
+    if(t){e.preventDefault();t.scrollIntoView({behavior:'smooth'});}
+  });
+});
